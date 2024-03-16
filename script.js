61a6dc314bcd6814e8f319193a01a9cd8b0f2ab2
ndieric <ndihokubwayoeric26@gmail.com>
2024-03-16 10:03:05 +0200

    some change

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ MenuBtn.addEventListener("click", function (e) {
   document.querySelector("body").classList.toggle("mobile-nav-active");
   this.classList.toggle("fa-xmark");
 });
+
+let typed = new typed(".auto-input", {
+  strings: ["Back-end Developer", "Front-end Developer", "and so on"],
+  typedpeed: 100,
+  backdpeed: 100,
+  backDelay: 2000,
+  loop: true,
+});
